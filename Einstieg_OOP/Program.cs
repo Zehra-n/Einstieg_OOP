9d655b34fcc4f000d38b74e34ba79f7b81054155
Zehra-n7 <zehranuhiu@gmail.com>
2024-12-04 09:02:47 +0100

    autos

--- Einstieg_OOP/Program.cs
+++ Einstieg_OOP/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Die drei Autos.");
 
-Console.WriteLine("Hello, World!");
+string[] auto1 = { "Audi", "Blau", "280" };
+Console.WriteLine($"Auto 1: Marke: {auto1[0]} | Farbe: {auto1[1]} | Höchstgeschwindigkeit (km/h): {auto1[2]}.");
+
+string[] auto2 = { "Porsche", "Gelb", "300" };
+Console.WriteLine($"Auto 2: Marke: {auto2[0]} | Farbe: {auto2[1]} | Höchstgeschwindigkeit (km/h): {auto2[2]}.");
+                
+string[] auto3 = { "Opel", "Rot", "220" };
+Console.WriteLine($"Auto 3: Marke: {auto3[0]} | Farbe: {auto3[1]} | Höchstgeschwindigkeit (km/h): {auto3[2]}.");
