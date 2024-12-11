e50f79f2ef2a32e678933f805fd26b2db3555d9c
Zehra-n7 <zehranuhiu@gmail.com>
2024-12-11 14:42:22 +0100

    demo

---
+++ Demo/Engine.cs
@@ -0,0 +1,44 @@
+﻿namespace Demo;
+
+public class Engine
+{
+    public double AmountLiter { get; }
+    public int Horsepower { get; }
+    public bool IsStarted { get; set; }
+
+    public Engine(double amountLiter, int horsepower)
+    {
+        this.AmountLiter = amountLiter;
+        this.Horsepower = horsepower;
+        this.IsStarted = false;
+    }
+
+    protected bool Equals(Engine other)
+    {
+        return AmountLiter.Equals(other.AmountLiter) && Horsepower == other.Horsepower;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        Engine other = (Engine) obj;
+        return this.AmountLiter.Equals(other.AmountLiter) && this.Horsepower == other.Horsepower;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(AmountLiter, Horsepower);
+    }
+
+    public override string ToString()
+    {
+        return $"AmountLiter: {AmountLiter}, Horsepower: {Horsepower}, Gestartet: {IsStarted}";
+    }
+
+    private Engine engine1 = new Engine(amountLiter: 1.8, horsepower: 100);
+    private Engine engine2 = new Engine(amountLiter: 2.0, horsepower: 200);
+    private Engine engine3 = new Engine(amountLiter: 1.8, horsepower: 100);
+
+}
---
+++ Tutorial/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Tutorial;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
